dc60ac015ac4e6cefc1f944d27680dc0d487fa86
MoepyaCode <kagisomoepya19@gmail.com>
2024-07-23 22:01:58 +0200

    🎉 First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Main } from './components'
+
+export default function App() {
+
+  return (
+    <Main className='bg-eggshell grid place-items-center min-w-full'>
+      <h1>Hello World</h1>
+    </Main>
+  )
+}
